65fbc9b6b3086a30ab4b036c55fcf568bcc15b7f
andrey776suarez <99951372+andrey776suarez@users.noreply.github.com>
2022-04-29 08:56:03 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showString("Rayo Maqueen cachau")
+basic.showLeds(`
+    # # # # #
+    # . # . #
+    # # # # #
+    . # # # .
+    . # # # .
+    `)
+basic.forever(function () {
+	
+})
